0dffa4183f58f29436c5feecb73ad3949d0cf50d
talal-web <maliktalal910@gmail.com>
2025-07-14 07:14:32 -0700

    Add Contact section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
     const navLinks = document.getElementById("nav-links")
     function showMenu(){
         navLinks.style.right = "0"
+        navLinks.style.display = "block"
     }
     function hideMenu(){
         navLinks.style.right  = "-200px"
